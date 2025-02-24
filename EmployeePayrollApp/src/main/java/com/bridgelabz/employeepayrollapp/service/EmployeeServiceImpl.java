ad04acbfe19489f8a4de9f42a6b2e59ada033d67
Deepansh_5 <104337001+DeepanshK5@users.noreply.github.com>
2025-02-24 15:46:21 +0530

    [Deepansh]Add . Implemented UC5: Store Employee Payroll data in memory (List)

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeServiceImpl.java
@@ -2,49 +2,50 @@ package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
-import com.bridgelabz.employeepayrollapp.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private List<Employee> employeeList = new ArrayList<>(); // Temporary storage
 
     @Override
     public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+        return employeeList; // Fetch from memory instead of database
     }
 
     @Override
     public Optional<Employee> getEmployeeById(Long id) {
-        return employeeRepository.findById(id);
+        return employeeList.stream().filter(emp -> emp.getId().equals(id)).findFirst();
     }
 
     @Override
     public Employee saveEmployee(EmployeeDTO employeeDTO) {
         Employee employee = new Employee(employeeDTO);
-        return employeeRepository.save(employee);
+        employee.setId((long) (employeeList.size() + 1)); // Manually assigning ID
+        employeeList.add(employee);
+        return employee;
     }
 
     @Override
     public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) {
-        Optional<Employee> existingEmployee = employeeRepository.findById(id);
-        if (existingEmployee.isPresent()) {
-            Employee employee = existingEmployee.get();
-            employee.setName(employeeDTO.getName());
-            employee.setSalary(employeeDTO.getSalary());
-            return employeeRepository.save(employee);
+        for (int i = 0; i < employeeList.size(); i++) {
+            if (employeeList.get(i).getId().equals(id)) {
+                Employee employee = employeeList.get(i);
+                employee.setName(employeeDTO.getName());
+                employee.setSalary(employeeDTO.getSalary());
+                return employee;
+            }
         }
         return null;
     }
 
     @Override
     public void deleteEmployee(Long id) {
-        employeeRepository.deleteById(id);
+        employeeList.removeIf(emp -> emp.getId().equals(id));
     }
 }
